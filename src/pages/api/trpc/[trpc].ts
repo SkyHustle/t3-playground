7798c2d4c146d5b596d70fd53f8428e2f2915d80
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-05 13:24:34 -0700

    Slightly simplified route handler using NextJS conventions

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,38 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
 // export API handler
-export default createNextApiHandler({
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+//   onError:
+//     env.NODE_ENV === "development"
+//       ? ({ path, error }) => {
+//           console.error(
+//             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+//           );
+//         }
+//       : undefined,
+// });
+
+// slightly simplified NextJS Api handler
+const nextApiHandler = createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
           );
         }
       : undefined,
 });
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return nextApiHandler(req, res);
+}
