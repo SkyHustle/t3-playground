5e9c516c1cf620ac158096a0068a18b31977dbac
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-06 15:26:36 -0700

    Post author should never be undefined

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,6 @@
 import type { User } from "@clerk/nextjs/api";
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 // import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -26,10 +27,20 @@ export const postRouter = createTRPCRouter({
       })
     ).map(filterUserForClient);
 
-    return posts.map((post) => ({
-      post,
-      author: users.find((user) => user.id === post.authorId),
-    }));
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author not found",
+        });
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 
   // hello: publicProcedure
