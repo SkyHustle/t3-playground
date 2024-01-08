a9c1249a2b566d2ff68fd047d296845c768c06d5
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-08 11:31:08 -0700

    Use upstash to ratelimit the amount of requests made, for now 3 per 1 minute is fine

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -8,6 +8,21 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 const filterUserForClient = (user: User) => {
   return {
@@ -57,6 +72,15 @@ export const postRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const { success } = await ratelimit.limit(ctx.userId);
+
+      if (!success) {
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+          message: "You are making too many requests too fast",
+        });
+      }
 
       return ctx.db.post.create({
         data: {
