3950373a69fa66427748042d782c55af02014935
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-09 17:39:48 -0700

    Extract post view component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import { SignInButton, useUser, UserButton } from "@clerk/nextjs";
-import { type RouterOutputs, api } from "~/utils/api";
-import Image from "next/image";
+import { api } from "~/utils/api";
 import { LoadingSpinner } from "~/components/LoadingSpinner";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import type { NextPage } from "next";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
+import PostView from "~/components/postview";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -68,37 +67,6 @@ const CreatePost: NextPage = () => {
       >
         {isPosting ? <LoadingSpinner /> : "Post"}
       </button>
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["post"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        width={48}
-        height={48}
-        src={author.profileImageUrl}
-        alt="Profile Image"
-        className="h-12 w-12 rounded-full"
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`· ${dayjs(
-              post.createdAt,
-            ).fromNow()} `}</span>
-          </Link>
-        </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
     </div>
   );
 };
